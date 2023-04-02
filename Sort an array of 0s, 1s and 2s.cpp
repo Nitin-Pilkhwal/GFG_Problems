cfe037c20f1c2707500168865ef15467fdb5c035
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-04-02 12:12:40 +0530

    Create Sort an array of 0s, 1s and 2s.cpp

---
+++ Sort an array of 0s, 1s and 2s.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+
+int sort012(int a[], int n)
+{
+    int i,zero=0,one=0;
+    for(i=0;i<n;i++){
+        if(a[i]==0) zero++;
+        else if(a[i]==1) one++;
+    }
+    for(i=0;i<n;i++){
+        if(zero) a[i]=0,zero--;
+        else if(one) a[i]=1,one--;
+        else a[i]=2;
+    }
+    return a;
+}
