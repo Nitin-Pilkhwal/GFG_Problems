4027b53a6ae1c2099a93180ff0b2784d75d5345f
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-05-08 21:39:30 +0530

    Create Pass by Reference and Value.cpp

---
+++ Pass by Reference and Value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> passedBy(int a, int &b) {
+        // code here
+        return {a+1,b+2};
+    }
+};
