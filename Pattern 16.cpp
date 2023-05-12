07a3ad969559275b574df5e90910dc72f890d8c1
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-05-12 22:10:07 +0530

    Create Pattern 16.cpp

---
+++ Pattern 16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        char c='A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++)
+                cout<<c;
+            c++;
+            cout<<endl;
+        }
+    }
+};
