4efbb724500cff93df1c5f0416018d60ce059c84
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-06-18 12:11:22 +0530

    Create Pattern 4.cpp

---
+++ Pattern 4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<i<<' ';
+            }
+            cout<<endl;
+        }
+    }
+};
